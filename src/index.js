38ade25cc2bf3305a6a9d2387ee3b6391cb5defa
Javiersalcedoj <javierss.2603@gmail.com>
2021-07-06 16:13:43 -0500

    npm

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const messages = [
+    "Oscar",
+    "Ana",
+    "Yesica",
+    "Diego",
+    "Laura",
+    "Carolina",
+    "Paulina"
+];
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random()*messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg };
